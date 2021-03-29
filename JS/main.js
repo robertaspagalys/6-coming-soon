b23b61150ed9e553cb623bb44f8b2023bb12e841
Robertas <robertux19@gmail.com>
2021-03-29 10:01:18 +0300

    update

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+import suma from './components/sum.js';
+import multiply from './components/multiply.js';
+
+
+const rez1 = suma(8, 5);
+const rez2 = multiply(8, 5);
+
+console.log(rez1);
+console.log(rez2);
